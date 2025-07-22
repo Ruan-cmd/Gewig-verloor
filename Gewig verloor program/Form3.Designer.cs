48c8a35f614a6b95efc93c96ae911671ad76be8b
Ruan-cmd <ruan4.t@gmail.com>
2025-07-23 00:25:53 +0200

    Add files via upload

---
+++ Gewig verloor program/Form3.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace Gewig_verloor_program
+{
+    partial class Form3
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Date = new System.Windows.Forms.TextBox();
+            this.Naam = new System.Windows.Forms.TextBox();
+            this.Weight = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(296, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(169, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Please enter the date :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(184, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Please enter your name :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(561, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(190, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Please enter your weight :";
+            // 
+            // Date
+            // 
+            this.Date.Location = new System.Drawing.Point(300, 103);
+            this.Date.Name = "Date";
+            this.Date.Size = new System.Drawing.Size(157, 20);
+            this.Date.TabIndex = 3;
+            // 
+            // Naam
+            // 
+            this.Naam.Location = new System.Drawing.Point(30, 103);
+            this.Naam.Name = "Naam";
+            this.Naam.Size = new System.Drawing.Size(180, 20);
+            this.Naam.TabIndex = 4;
+            // 
+            // Weight
+            // 
+            this.Weight.Location = new System.Drawing.Point(565, 94);
+            this.Weight.Name = "Weight";
+            this.Weight.Size = new System.Drawing.Size(186, 20);
+            this.Weight.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(182, 158);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(376, 25);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "After every day press the save button.";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(200, 214);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(335, 60);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Save";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(200, 351);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(335, 66);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(200, 280);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(335, 66);
+            this.button3.TabIndex = 9;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form3
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Weight);
+            this.Controls.Add(this.Naam);
+            this.Controls.Add(this.Date);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form3";
+            this.Text = "Form3";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Date;
+        private System.Windows.Forms.TextBox Naam;
+        private System.Windows.Forms.TextBox Weight;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
---
+++ Gewig verloor program/Form3.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Gewig_verloor_program
+{
+    public partial class Form3 : Form
+    {
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Naam.Text == "Kete")
+            {
+                string textBoxContent = Date.Text  + ("    ") + Weight.Text;
+
+                // Specify the file path where you want to write the content
+                string filePath = "A:\\Gewig verloor\\Kete\\Gewig.txt";
+
+                try
+                {
+                    // Use StreamWriter to write the content to the file
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                        writer.WriteLine(textBoxContent);
+                    }
+
+                    MessageBox.Show("Hy het na die file toe geskryf");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error writing to file: {ex.Message}");
+                }
+                
+            }
+            else if (Naam.Text == "Ruan")
+            {
+                string textBoxContent = Date.Text + ("    ") + Weight.Text;
+
+                // Specify the file path where you want to write the content
+                string filePath = "A:\\Gewig verloor\\Ruan\\Gewig.txt";
+
+                try
+                {
+                    // Use StreamWriter to write the content to the file
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                        writer.WriteLine(textBoxContent);
+                    }
+                    MessageBox.Show("Hy het na die file toe geskryf");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error writing to file: {ex.Message}");
+                }
+                
+            }
+            
+            else if (Naam.Text == "Inge")
+            {
+                string textBoxContent = Date.Text + ("    ") + Weight.Text;
+
+                // Specify the file path where you want to write the content
+                string filePath = "A:\\Gewig verloor\\Inge\\Gewig.txt";
+
+                try
+                {
+                    // Use StreamWriter to write the content to the file
+                    using (StreamWriter writer = new StreamWriter(filePath, true))
+                    {
+                        writer.WriteLine(textBoxContent);
+                    }
+
+                    MessageBox.Show("Hy het na die file toe geskryf");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show($"Error writing to file: {ex.Message}");
+                }
+                
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Naam.Clear();
+            Weight.Clear();
+            Naam.Focus();
+        }
+    }
+}
